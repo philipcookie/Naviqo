04c44ee7eb0c2164d760576d07ce577d502d4d44
Philip Cooke <philipjcooke@gmail.com>
2016-09-21 20:19:59 -0500

    set up mongo and seeded database

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var mongoose = require('mongoose');
 
 app.use(express.static(path.normalize(path.join(__dirname, 'public'))));
 var PORT = process.env.PORT || 8080;//must use this for heroku deployment
@@ -17,16 +18,28 @@ app.set('view engine', 'handlebars');
 //SET UP BODYPARSER
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
+//MAKE STATIC PUBLIC DIRECTORY
 app.use('/public', express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// SETTING UP DATABASE CONNECTION & Mongoose
+mongoose.connect('mongodb://localhost/serviceProviders');
+var db = mongoose.connection;
+
+db.on('error', function(err){
+	console.log('Mongoose connnection successful.');
+});
+
+var Provider = require('./models/ServiceProvider.js');
+var Requestor = require('./models/ServiceRequestor.js');
 
 //ROUTES
 var homeController = require('./controllers/homeController.js');
 app.use('/', homeController);
 
+// var searchResults = require('./controllers/searchResults.js');
+// app.use('/search', searchResults);
 
 
 app.listen(PORT, function(){
