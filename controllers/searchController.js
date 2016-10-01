b9e66dc7d9c096a825bf1c660a5605eafa463678
Philip Cooke <philipjcooke@gmail.com>
2016-10-01 17:19:29 -0500

    fixed routes to call database for all business lines

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -8,18 +8,81 @@ var Serviceproviders = require('../models/Serviceproviders.js');
 
 router.post('/AF', function(req, res){
 	
-	console.log('You have arrived');
-	console.log(req.body);
+	//console.log('You have arrived');
+	var path = (req.body.busLine);
+	// console.log(path);
 	
 	Serviceproviders.find({}, (err, result) => { 
 		
-		//console.log(result);
-
 		if (err) {
 			throw (err);
 		} 
-		//console.log(result);
-		var hbsObj = {provider : result}
+		
+		console.log(result[0].path_AF);		
+		var search = [];
+		//console.log(searchParam);
+
+		for (var i = 0; i < result.length; i++) {
+	 		if(result[i].path_AF == true && result[i].SP_Firm_City == 'Austin') {
+	 			search.push(result[i]);
+	 		}
+	 	};
+
+		var hbsObj = {provider : search}
+		res.render('search', hbsObj);
+	});
+});
+
+router.post('/Legal', function(req, res){
+	
+	//console.log('You have arrived');
+	var path = (req.body.busLine);
+	// console.log(path);
+	
+	Serviceproviders.find({}, (err, result) => { 
+		
+		if (err) {
+			throw (err);
+		} 
+		
+		console.log(result[0].path_Legal);		
+		var search = [];
+		//console.log(searchParam);
+
+		for (var i = 0; i < result.length; i++) {
+	 		if(result[i].path_Legal == true && result[i].SP_Firm_City == 'Austin') {
+	 			search.push(result[i]);
+	 		}
+	 	};
+
+		var hbsObj = {provider : search}
+		res.render('search', hbsObj);
+	});
+});
+
+router.post('/IT', function(req, res){
+	
+	//console.log('You have arrived');
+	var path = (req.body.busLine);
+	// console.log(path);
+	
+	Serviceproviders.find({}, (err, result) => { 
+		
+		if (err) {
+			throw (err);
+		} 
+		
+		console.log(result[0].path_IT);		
+		var search = [];
+		//console.log(searchParam);
+
+		for (var i = 0; i < result.length; i++) {
+	 		if(result[i].path_IT == true && result[i].SP_Firm_City == 'Austin') {
+	 			search.push(result[i]);
+	 		}
+	 	};
+
+		var hbsObj = {provider : search}
 		res.render('search', hbsObj);
 	});
 });
