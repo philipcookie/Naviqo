60e2e6fc02209d906379d894a54c6e3a8bf1877b
Philip Cooke <philipjcooke@gmail.com>
2016-10-05 15:51:51 -0500

    implemented overhangjs

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,10 +2,20 @@ console.log("I AM LINKED");
 
 $(document).ready(function() {
 
+	var count = 0;
+
 	$('input:checkbox').change(function(){
 		if($(this).is(":checked")) {
 			$(this).parent().closest('div.search_result').addClass("selected");
+			count++;
+			$("body").overhang({
+				custom: true, // Set custom to true
+				message : "Request a proposal from " + count + " vendors.",
+				primary: "#116990", // Your custom primary color
+				accent: "#FC9923", // Your custom accent color
+				});
 		} else {
+			count--;
 			$(this).parent().closest('div.search_result').removeClass("selected");
 		}
 	});
